e49aa890ab762f9fdf263fb8d1bd7cb7ba4c9818
Xavier Jenkins <mjom1999@gmail.com>
2021-10-30 21:03:20 -0400

    initial pie chart

--- src/Heatmap.js
+++ src/Heatmap.js
@@ -29,20 +29,22 @@ class myChart extends Component {
                 rootProps={{ 'data-testid': '1' }} />
             );
         };
-    }
+    }/*
     componentDidMount(){
+        globalVar.clear_state();
         axios.get(`https://election2020api.herokuapp.com/api/results`).then(results => {
             axios.get('https://election2020api.herokuapp.com/api/states/').then(states => {
                 var output = []
                 var country_data = []
                 states.data.forEach((state) => {
                     results.data.forEach((resultData) => {
-                        if (state.stateabrv === resultData.stateid){
+                        if (state.stateid === resultData.stateid){
                             country_data.push({demvotes: resultData.demvotes, repvotes: resultData.repvotes, othervotes: resultData.othervotes})
                         }
                     });
                 });
                 
+                // globalVar.update_stats({});
                 var demvotes = 0
                 var repvotes = 0
                 var othervotes = 0
@@ -53,19 +55,35 @@ class myChart extends Component {
                     othervotes += data.othervotes
                 });
 
-                globalVar.update_stats({title: "U.S. 2020 Election Results", "Democratic Votes": demvotes.toLocaleString(), "Republican Votes": repvotes.toLocaleString(), "Other Votes": othervotes.toLocaleString()})
+                globalVar.update_stats({title: "U.S 2020 Election Results", "Democratic Votes": demvotes.toLocaleString(), "Republican Votes": repvotes.toLocaleString(), "Other Votes": othervotes.toLocaleString()})
 
             });
         });
-    }
+    }*/
     
 
 
     
     render() {
         return (
-            <chart />
-        );
+            <Chart
+                width={'500px'}
+                height={'300px'}
+                chartType="PieChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                ['Task', 'Hours per Day'],
+                ['Work', 11],
+                ['Eat', 2],
+                ['Commute', 2],
+                ['Watch TV', 2],
+                ['Sleep', 7],
+                ]}
+                options={{
+                title: 'My Daily Activities'
+                }}
+                rootProps={{ 'data-testid': '1' }} />
+        )
     };
 };
 
